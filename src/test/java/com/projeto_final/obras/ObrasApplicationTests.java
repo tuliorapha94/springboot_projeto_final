5d256994a196f79a70141e3326774d73fb97ae1a
Tharcyl <tharcylhenrique@gmail.com>
2022-11-22 13:52:56 -0300

    first commit

---
+++ src/test/java/com/projeto_final/obras/ObrasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto_final.obras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObrasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
